354fd636a3f62b25f2bc7cc2ae2af937fec7a751
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-22 15:06:47 +0400

    created sign-in and sign-up classes

---
+++ pages/sign-in.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { SignIn } from "vue-clerk";
+
+definePageMeta({
+  middleware: ["public"],
+  title: "ai buddy | Sign-in",
+  layout: "auth",
+});
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <SignIn sign-up-url="/sign-up" />
+  </div>
+</template>
---
+++ pages/sign-up.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { SignUp } from "vue-clerk";
+definePageMeta({
+  layout: "auth",
+  middleware: ["public"],
+  title: "Taskify | Sign-up",
+});
+</script>
+
+<template>
+  <div class="grid h-full place-items-center">
+    <SignUp sign-in-url="/sign-in" />
+  </div>
+</template>
