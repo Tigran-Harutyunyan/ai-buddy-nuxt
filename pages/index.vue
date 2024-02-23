f9a0ce20c2cdb031b769fc80abfa396516f3d78d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-23 18:22:59 +0400

    created index page that hosts search, categories and companions components

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import qs from "query-string";
+
+const { data: categories } = useLazyAsyncData("categories", () =>
+  $fetch("/api/categories")
+);
+
+const getUrl = () => {
+  return qs.stringifyUrl(
+    {
+      url: "/api/companions",
+      query: useRoute().query,
+    },
+    { skipNull: true, skipEmptyString: true }
+  );
+};
+
+const { data: companions, refresh } = useLazyAsyncData("companions", () =>
+  $fetch(getUrl())
+);
+
+const data = ref(companions);
+
+watch(
+  () => useRoute().query,
+  () => {
+    refresh();
+  }
+);
+</script>
+<template>
+  <div class="h-full p-4 space-y-2">
+    <SearchInput />
+    <Categories :data="categories" />
+    <Companions :data="data" />
+  </div>
+</template>
