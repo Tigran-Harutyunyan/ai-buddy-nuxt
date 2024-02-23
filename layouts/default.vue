ca6768ba6b8c98c7c5295959342e571380d34f88
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-23 14:49:13 +0400

    added functionality to default layout

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ClerkLoaded, ClerkLoading } from "vue-clerk";
+import Loading from "~/components/Loading.vue";
+import Navbar from "@/components/Navbar.vue";
+import Sidebar from "@/components/Sidebar.vue";
+useHead({
+  bodyAttrs: {
+    class: "bg-secondary",
+  },
+});
+</script>
+<template>
+  <ClerkLoading> </ClerkLoading>
+  <ClerkLoaded>
+    <div class="h-full">
+      <Navbar />
+      <div class="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
+        <Sidebar />
+      </div>
+      <main class="md:pl-20 pt-16 h-full">
+        <slot />
+      </main>
+    </div>
+  </ClerkLoaded>
+</template>
