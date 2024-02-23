f4b27dff9c275aa3ba716aa6e8ecd701e5e1d2d0
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-23 14:44:22 +0400

    created MobileSidebar component

---
+++ components/MobileSidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-vue-next";
+import Sidebar from "@/components/Sidebar.vue";
+const { isPro } = defineProps<{
+  isPro?: boolean;
+}>();
+</script>
+
+<template>
+  <Sheet>
+    <SheetTrigger class="md:hidden pr-4">
+      <Menu />
+    </SheetTrigger>
+    <SheetContent side="left" class="p-0 bg-secondary pt-10 w-32">
+      <Sidebar :isPro="isPro" />
+    </SheetContent>
+  </Sheet>
+</template>
