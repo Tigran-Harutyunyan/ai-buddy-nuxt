4ebe9724c04f5f440dc041755a63f19df3df22fc
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-23 14:45:46 +0400

    created Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { Sparkles } from "lucide-vue-next";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "vue-clerk";
+import ModeToggle from "@/components/ModeToggle.vue";
+import MobileSidebar from "@/components/MobileSidebar.vue";
+
+interface NavbarProps {
+  isPro?: boolean;
+}
+
+const { isPro } = defineProps<NavbarProps>();
+</script>
+
+<template>
+  <div
+    class="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary"
+  >
+    <div class="flex items-center">
+      <MobileSidebar :isPro="isPro" />
+      <NuxtLink to="/">
+        <h1
+          class="hidden md:block text-xl md:text-3xl font-bold text-primary font-poppins"
+        >
+          companion.ai
+        </h1>
+      </NuxtLink>
+    </div>
+    <div class="flex items-center gap-x-3">
+      <Button v-if="!isPro" @click="" size="sm" variant="premium">
+        Upgrade
+        <Sparkles class="h-4 w-4 fill-white text-white ml-2" />
+      </Button>
+      <ModeToggle />
+      <UserButton after-sign-out-url="/" />
+    </div>
+  </div>
+</template>
