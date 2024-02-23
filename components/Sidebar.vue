430d5d07ec8c5b42d28f9546f4bb98188a8c9554
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-23 14:43:35 +0400

    created Sidebar component

---
+++ components/Sidebar.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+import { Home, Plus, Settings } from "lucide-vue-next";
+
+interface SidebarProps {
+  isPro?: boolean;
+}
+
+const router = useRouter();
+
+const routes = [
+  {
+    icon: Home,
+    href: "/",
+    label: "Home",
+    pro: false,
+  },
+  {
+    icon: Plus,
+    href: "/companion/new",
+    label: "Create",
+    pro: true,
+  },
+  {
+    icon: Settings,
+    href: "/settings",
+    label: "Settings",
+    pro: false,
+  },
+];
+const onNavigate = (url: string, pro: boolean) => {
+  if (pro && !isPro) {
+    return; //proModal.onOpen();
+  }
+
+  return router.push(url);
+};
+const { isPro } = defineProps<SidebarProps>();
+
+const currentRoute = useRoute();
+</script>
+
+<template>
+  <div class="space-y-4 flex flex-col h-full text-primary bg-secondary">
+    <div class="p-3 flex-1 flex justify-center">
+      <div class="space-y-2">
+        <div
+          v-for="route in routes"
+          @click="onNavigate(route.href, route.pro)"
+          :key="route.href"
+          :class="
+            cn(
+              'text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition',
+              currentRoute.path === route.href && 'bg-primary/10 text-primary'
+            )
+          "
+        >
+          <div class="flex flex-col gap-y-2 items-center flex-1">
+            <component :is="route.icon" class="h-5 w-5" />
+            {{ route.label }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
