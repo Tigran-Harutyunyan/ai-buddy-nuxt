5a64682f7454ea6c6fe6428ff2e96c72e094c6eb
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-24 10:11:17 +0400

    created Bot and User avatar components

---
+++ components/BotAvatar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
+interface BotAvatarProps {
+  src: string;
+}
+
+const { src } = defineProps<BotAvatarProps>();
+</script>
+
+<template>
+  <Avatar class="h-12 w-12">
+    <AvatarImage :src="src" />
+  </Avatar>
+</template>
---
+++ components/UserAvatar.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+</script>
+
+<template>
+  <Avatar class="h-12 w-12">
+    <AvatarImage :src="user?.imageUrl" />
+  </Avatar>
+</template>
