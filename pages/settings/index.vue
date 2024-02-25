0273a18a511c64506d92f75f46de721f1f9ec999
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-25 14:17:35 +0400

    created Settings page

--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 definePageMeta({ middleware: "auth" });
+const { isPro } = inject("isPro");
 </script>
 
 <template>
-  <div>settings</div>
+  <div class="h-full p-4 space-y-2">
+    <h3 class="text-lg font-medium">Settings</h3>
+    <div class="text-muted-foreground text-sm">
+      {{
+        isPro
+          ? "You are currently on a Pro plan."
+          : "You are currently on a free plan."
+      }}
+    </div>
+    <SubscriptionButton :isPro="isPro" />
+  </div>
 </template>
