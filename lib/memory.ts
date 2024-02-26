6757857cb3100471cdcaecc8adc0b31ec1744d95
Tigran <harutyunyan.tigran1975@gmail.com>
2024-02-26 15:02:33 +0400

    updated memory management with updated libs

--- lib/memory.ts
+++ lib/memory.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 import { OpenAIEmbeddings } from "@langchain/openai";
-import { PineconeClient } from "@pinecone-database/pinecone";
+import { Pinecone } from "@pinecone-database/pinecone";
 import { PineconeStore } from "@langchain/pinecone";
 
 export type CompanionKey = {
@@ -12,32 +12,25 @@ export type CompanionKey = {
 export class MemoryManager {
   private static instance: MemoryManager;
   private history: Redis;
-  private vectorDBClient: PineconeClient;
+  private vectorDBClient: Pinecone;
 
   public constructor() {
     this.history = Redis.fromEnv();
-    this.vectorDBClient = new PineconeClient();
+    this.vectorDBClient = new Pinecone({
+      apiKey: process.env.PINECONE_API_KEY!
+    });
   }
-
-  public async init() {
-    if (this.vectorDBClient instanceof PineconeClient) {
-      await this.vectorDBClient.init({
-        apiKey: process.env.PINECONE_API_KEY!,
-        environment: process.env.PINECONE_ENVIRONMENT!,
-      });
-    }
-  }
-
+ 
   public async vectorSearch(
     recentChatHistory: string,
     companionFileName: string
   ) {
-    const pineconeClient = <PineconeClient>this.vectorDBClient;
+    const pineconeClient = <Pinecone>this.vectorDBClient;
 
     const pineconeIndex = pineconeClient.Index(
       process.env.PINECONE_INDEX! || ""
-    ); 
-    
+    );
+
     const vectorStore = await PineconeStore.fromExistingIndex(
       new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY }),
       { pineconeIndex }
@@ -54,7 +47,6 @@ export class MemoryManager {
   public static async getInstance(): Promise<MemoryManager> {
     if (!MemoryManager.instance) {
       MemoryManager.instance = new MemoryManager();
-      await MemoryManager.instance.init();
     }
     return MemoryManager.instance;
   }
